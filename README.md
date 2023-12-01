b8fea30a4f673edb29c486f859e7e7921e69af62
Madeliene <madelienestiller@gmail.com>
2023-12-01 11:25:25 +1000

    updates to app features and README file creation

---
+++ README.md
@@ -0,0 +1,44 @@
+# Beer Finder
+
+This project is a simple single page web application that allows users to search for beers using the Punk API. Users can filter beers by their Alcohol By Volume (ABV) values and search for beers by name. The can also 'Like' beer using a button.
+
+## Features
+
+- Search for beers by name
+- Filter beers by ABV
+- Like a beer
+- View basic information about each beer including name, tagline, ABV and description
+
+
+## How to Use
+
+1. Enter a beer name in the search box.
+2. Adjust the ABV range using the sliders. There are 2 sliders, one to set the min ABV and one to set the min ABV
+3. Click the "Search" button to display the beers that match your search criteria.
+4. The search results will display the selected beer's card
+5. You can click the "Like" button on each beer card to mark it as liked.
+
+
+# Note on the Search Function
+
+While the search function has been implemented to the API documentation, it can sometimes show confusing results.
+
+## Technologies Used
+
+- HTML
+- CSS
+- JavaScript
+- Punk API
+
+## Project Setup
+
+To set up the project on your local machine, follow these steps:
+
+1. Clone the repository to your local machine.
+2. Open the `index.html` file in your web browser.
+
+## Contributing
+
+Contributions are welcome.
+
+## License
--- script.js
+++ script.js
@@ -1,12 +1,26 @@
 // Use const for baseUrl since it's not going to change
 const baseUrl = 'https://api.punkapi.com/v2/beers';
 
+
+document.addEventListener('DOMContentLoaded', () => {
+    const abvMinLabel = document.getElementById('abv-min-label');
+    const abvMaxLabel = document.getElementById('abv-max-label');
+
+    filterAbvMin.addEventListener('input', () => {
+        abvMinLabel.textContent = `Min: ${filterAbvMin.value}`;
+    });
+
+    filterAbvMax.addEventListener('input', () => {
+        abvMaxLabel.textContent = `Max: ${filterAbvMax.value}`;
+    });
+});
+
 // Use arrow function syntax for functions
 const displayBeers = beers => {
     const beerContainer = document.getElementById('beer-container');
     beerContainer.innerHTML = ''; // clear the container
 
-    beers.forEach(({ image_url, name, tagline, abv }) => { // Destructure beer properties
+    beers.forEach(({ image_url, name, tagline, abv, description }) => { // Destructure beer properties
         const beerCard = document.createElement('div');
         beerCard.className = 'beer-card';
         beerCard.innerHTML = `
@@ -14,6 +28,7 @@ const displayBeers = beers => {
             <h2>${name}</h2>
             <h3>${tagline}</h3>
             <p><strong>ABV:</strong> ${abv}%</p>
+            <p>${description}</p>
             <button class="like-button">Like</button>`;
         beerContainer.appendChild(beerCard);
     });
@@ -34,9 +49,18 @@ const [filterAbvMax, filterAbvMin,searchBox] = ['filter-abv-max', 'filter-abv-mi
 
 // Use Promises for asynchronous operations
 const getBeers = () => {
-    const abvMax = filterAbvMax.value;
-    const abvMin = filterAbvMin.value;
-    const beerName = searchBox.value;
+    let abvMax = parseInt(filterAbvMax.value);
+    let abvMin = parseInt(filterAbvMin.value);
+
+    // Validate and constrain abvMin and abvMax
+    if (abvMin < 0 || abvMin > 25) {
+        console.error('abvMin must be between 0 and 25');
+        return;
+    }
+    if (abvMax < 26 || abvMax > 55) {
+        console.error('abvMax must be between 26 and 55');
+        return;
+    }
 
     const params = {
         abv_gt: abvMin,
@@ -44,6 +68,7 @@ const getBeers = () => {
     };
 
     // Only add the beer_name parameter if it's not an empty string
+    const beerName = searchBox.value;
     if (beerName.trim() !== '') {
         params.beer_name = beerName;
     }
@@ -64,8 +89,10 @@ const getBeers = () => {
 
 
 const searchButton = document.getElementById('search-button');
-searchButton.addEventListener('click', getBeers);
-
+searchButton.addEventListener('click', () => {
+    getBeers();
+    searchBox.value = '';
+});
 
 
 getBeers()
