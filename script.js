e3d4b97decd0f64237f08b1cd7b1be6ae5ade56c
Madeliene <madelienestiller@gmail.com>
2023-12-01 12:33:02 +1000

    Added JSDoc and added additional comments

--- script.js
+++ script.js
@@ -1,34 +1,138 @@
-// Use const for baseUrl since it's not going to change
+/**
+ * Base URL for the API. This is the endpoint from which we fetch the beer data.
+ * @type {string}
+ * @const
+ */
 const baseUrl = 'https://api.punkapi.com/v2/beers';
 
+/**
+ * Function to like a beer. This function makes a POST request to the server to add the beer to the likedBeers list.
+ * @param {number} id - The id of the beer to be liked.
+ */
 const likeBeer = (id) => {
-    // Make a POST request to add the beer to the likedBeers list
+    // Make a POST request to the server
     fetch('http://localhost:3000/likedBeers', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
+        // The body of the request is the id of the beer to be liked
         body: JSON.stringify({ id: id })
     });
 }
 
+/**
+ * Function to get liked beers. This function fetches the ids of the liked beers from the server, 
+ * then fetches the data for these beers from the API. Both requests are chained using the Promise API. 
+ * It then displays the beers using the {@link displayBeers} function.
+ */
 const getLikedBeers = () => {
+    // Fetch the ids of the liked beers from the server
     fetch('http://localhost:3000/likedBeers')
         .then(response => response.json())
         .then(ids => {
-            const idsSring = ids.reduce((acc, { id }) => acc + `${id}|`, '');
-            const urlParams = new URLSearchParams({ ids: idsSring });
-
-            // Append params to the base URL
-            fetch(`${baseUrl}?${urlParams.toString()}`)
-                .then(response => response.json())
-                .then(beers => displayBeers(beers))
-                .catch(error => console.error('Error:', error));
-
+            // Create a string of ids separated by '|'
+            const idsString = ids.reduce((acc, { id }) => acc + (acc ? '|' : '') + id, '');
+            // Create a URLSearchParams object with the ids
+            const urlParams = new URLSearchParams({ ids: idsString });
+            // Fetch the beer data from the API
+            return fetch(`${baseUrl}?${urlParams.toString()}`);
         })
+        .then(response => response.json())
+        // Display the beers using the displayBeers function
+        .then(beers => displayBeers(beers))
         .catch(error => console.error('Error:', error));
 }
 
+/**
+ * Function to display beers. This function creates a card for each beer and appends it to the beer container.
+ * @param {Array} beers - The array of beers to display.
+ */
+const displayBeers = beers => {
+    // Get the beer container element
+    const beerContainer = document.getElementById('beer-container');
+    // Clear the beer container
+    beerContainer.innerHTML = '';
+
+    // For each beer, create a card and append it to the beer container
+    beers.forEach(({ image_url, name, tagline, abv, description, id }) => {
+        // Create a div for the beer card
+        const beerCard = document.createElement('div');
+        // Create a button for liking the beer
+        const likeButton = document.createElement('button');
+        likeButton.className = 'like-button';
+        likeButton.id = id;
+        likeButton.textContent = 'Like';
+
+        // Add an event listener to the like button
+        likeButton.addEventListener('click', event => {
+            // If the beer is not already liked, like it
+            if (!likeButton.classList.contains('liked')) {
+                likeBeer(event.target.id)
+                likeButton.textContent = 'Liked';
+                likeButton.classList.add('liked');
+            }
+        })
+
+        // Set the class and inner HTML of the beer card
+        beerCard.className = 'beer-card';
+        beerCard.innerHTML = `
+            <img src="${image_url}">
+            <h2>${name}</h2>
+            <h3>${tagline}</h3>
+            <p><strong>ABV:</strong> ${abv}%</p>
+            <p>${description}</p>`;
+        // Append the like button to the beer card
+        beerCard.appendChild(likeButton);
+        // Append the beer card to the beer container
+        beerContainer.appendChild(beerCard);
+    });
+}
+
+/**
+ * Function to get beers. This function fetches beer data from the API based on the user's input, then displays the beers using the displayBeers function.
+ */
+const getBeers = () => {
+    // Get the ABV values from the input elements
+    let abvMax = parseInt(filterAbvMax.value);
+    let abvMin = parseInt(filterAbvMin.value);
+
+    // Validate the ABV values
+    if (abvMin < 0 || abvMin > 25) {
+        console.error('abvMin must be between 0 and 25');
+        return;
+    }
+    if (abvMax < 26 || abvMax > 55) {
+        console.error('abvMax must be between 26 and 55');
+        return;
+    }
+
+    // Create an object with the ABV values
+    const params = {
+        abv_gt: abvMin,
+        abv_lt: abvMax
+    };
+
+    // If the user has entered a beer name, add it to the params object
+    const beerName = searchBox.value;
+    if (beerName.trim() !== '') {
+        params.beer_name = beerName;
+    }
+
+    // Create a URLSearchParams object with the params
+    const urlParams = new URLSearchParams(params);
+
+    // Fetch the beer data from the API
+    fetch(`${baseUrl}?${urlParams.toString()}`)
+        .then(response => response.json())
+        .then(beers => displayBeers(beers))
+        .catch(error => console.error('Error:', error));
+}
+
+/**
+ * This function is executed when the DOM is fully loaded. It sets up event listeners on the ABV filter input elements.
+ * The event listeners update the text content of the corresponding labels whenever the input value changes.
+ */
 document.addEventListener('DOMContentLoaded', () => {
     const abvMinLabel = document.getElementById('abv-min-label');
     const abvMaxLabel = document.getElementById('abv-max-label');
@@ -42,97 +146,27 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 });
 
-// Use arrow function syntax for functions
-const displayBeers = beers => {
-    const beerContainer = document.getElementById('beer-container');
-    beerContainer.innerHTML = ''; // clear the container
-
-    beers.forEach(({ image_url, name, tagline, abv, description, id }) => { // Destructure beer properties
-        const beerCard = document.createElement('div');
-        const likeButton = document.createElement('button');
-        likeButton.className = 'like-button';
-        likeButton.id = id;
-        likeButton.textContent = 'Like';
-
-        likeButton.addEventListener('click', event => {
-            console.log({ fireEvent: event.target })
-            if (!likeButton.classList.contains('liked')) {
-                likeBeer(event.target.id)
-                likeButton.textContent = 'Liked';
-                likeButton.classList.add('liked');
-            }
-        })
-
-
-        beerCard.className = 'beer-card';
-        beerCard.innerHTML = `
-            <img src="${image_url}">
-            <h2>${name}</h2>
-            <h3>${tagline}</h3>
-            <p><strong>ABV:</strong> ${abv}%</p>
-            <p>${description}</p>`;
-        beerCard.appendChild(likeButton);
-        beerContainer.appendChild(beerCard);
-
-
-    });
-
-    // Use event delegation to handle like button clicks
-    // This reduces the number of event listeners and improves performance
-
-}
-
-// Use destructuring to get filter elements
+/**
+ * Get the DOM elements for the ABV filters and the search box.
+ * @type {Array.<HTMLElement>}
+ */
 const [filterAbvMax, filterAbvMin, searchBox] = ['filter-abv-max', 'filter-abv-min', 'search-box'].map(id => document.getElementById(id));
 
-
-
-// Use Promises for asynchronous operations
-const getBeers = () => {
-    let abvMax = parseInt(filterAbvMax.value);
-    let abvMin = parseInt(filterAbvMin.value);
-
-    // Validate and constrain abvMin and abvMax
-    if (abvMin < 0 || abvMin > 25) {
-        console.error('abvMin must be between 0 and 25');
-        return;
-    }
-    if (abvMax < 26 || abvMax > 55) {
-        console.error('abvMax must be between 26 and 55');
-        return;
-    }
-
-    const params = {
-        abv_gt: abvMin,
-        abv_lt: abvMax
-    };
-
-    // Only add the beer_name parameter if it's not an empty string
-    const beerName = searchBox.value;
-    if (beerName.trim() !== '') {
-        params.beer_name = beerName;
-    }
-
-    // Convert params object to a URLSearchParams instance
-    const urlParams = new URLSearchParams(params);
-    console.log({ urlParams })
-
-    // Append params to the base URL
-    fetch(`${baseUrl}?${urlParams.toString()}`)
-        .then(response => response.json())
-        .then(beers => displayBeers(beers))
-        .catch(error => console.error('Error:', error));
-}
-
-// Add event listeners in a loop to reduce repetition
+// Add 'change' event listeners to the ABV filters. When the value of a filter changes, the getBeers function is called.
 [filterAbvMax, filterAbvMin].forEach(filter => filter.addEventListener('change', getBeers));
 
-
+/**
+ * Get the DOM element for the search button and add a 'click' event listener.
+ * When the search button is clicked, the getBeers function is called and the search box is cleared.
+ */
 const searchButton = document.getElementById('search-button');
 searchButton.addEventListener('click', () => {
     getBeers();
     searchBox.value = '';
 });
 
+// Add a 'click' event listener to the 'liked-beers-button'. When the button is clicked, the getLikedBeers function is called.
 document.getElementById('liked-beers-button').addEventListener('click', getLikedBeers);
+
+// Call the getBeers function when the script is loaded to display the initial list of beers.
 getBeers()
